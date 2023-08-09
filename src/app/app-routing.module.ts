77150051f0358899d76bfa744055049e079fe61e
Ardhiyan Pratama <ardhiyanp@gmail.com>
2023-08-09 08:42:45 +0700

    first init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonsComponent } from './components/persons/persons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PersonsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
